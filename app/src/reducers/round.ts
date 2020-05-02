78ca6c472ac9836df409e30182abccff9d2d2fa4
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-05-02 09:52:43 +0200

    shuffle cards on reveal

--- app/src/reducers/round.ts
+++ app/src/reducers/round.ts
@@ -1,6 +1,8 @@
 import { IRound } from "../interfaces";
 import { RoundAction, RoundActionTypes } from "../actions/round";
 
+import { shuffle } from "../utils";
+
 export default function(state: IRound | null = null, action: RoundAction) {
   switch (action.type) {
     case RoundActionTypes.START_ROUND:
@@ -16,9 +18,9 @@ export default function(state: IRound | null = null, action: RoundAction) {
     case RoundActionTypes.RECEIVE_ANSWER:
       return state ? { ...state, answers: [...state.answers, action.payload] } : null;
     case RoundActionTypes.REVEAL_ANSWERS:
-      return state ? { ...state, answersRevealed: true } : null;
+      return state ? { ...state, answersRevealed: true, answers: shuffle(state.answers) } : null;
     case RoundActionTypes.RECEIVE_ANSWERS_REVEALED:
-      return state ? { ...state, answersRevealed: true } : null;
+      return state ? { ...state, answersRevealed: true, answers: shuffle(state.answers) } : null;
     case RoundActionTypes.RECEIVE_WINNER:
       return state ? { ...state, winner: action.payload } : null;
     default:
