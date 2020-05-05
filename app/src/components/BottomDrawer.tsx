db35503675a97c0c37806967106b0da5ec986d9f
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-05-05 17:39:35 +0200

    Revert "make bottom drawer swipeable"
    This reverts commit 0942b3869d860cf38bb15cc89a84b8415b784f27.

--- app/src/components/BottomDrawer.tsx
+++ app/src/components/BottomDrawer.tsx
@@ -1,31 +1,22 @@
 import React from "react";
-import { SwipeableDrawer } from "@material-ui/core";
+import { Drawer } from "@material-ui/core";
 import styled, { AnyStyledComponent } from "styled-components";
 
 interface IBottomDrawerProps {
   open: boolean;
-  onOpen?: () => void;
-  onClose?: () => void;
+  onClick: () => void;
 }
 
 class BottomDrawer extends React.PureComponent<IBottomDrawerProps, {}> {
-  showDrawer = () => {
-    if (this.props.onOpen) {
-      this.props.onOpen();
-    }
-  };
-
   hideDrawer = () => {
-    if (this.props.onClose) {
-      this.props.onClose();
-    }
+    this.props.onClick();
   };
 
   render = () => {
     return (
-      <SwipeableDrawer anchor="bottom" open={this.props.open} onOpen={this.showDrawer} onClose={this.hideDrawer}>
+      <Drawer anchor="bottom" variant="temporary" open={this.props.open} onClick={this.hideDrawer}>
         <DrawerContent>{this.props.children}</DrawerContent>
-      </SwipeableDrawer>
+      </Drawer>
     );
   };
 }
