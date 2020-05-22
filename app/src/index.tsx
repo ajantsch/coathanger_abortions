9e5f190e7a658c703b2e99370dfc3e687e130f66
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-05-22 14:14:54 +0200

    cleanup

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -19,10 +19,10 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("/service-worker.js")
       .then(registration => {
-        console.log("SW registered: ", registration);
+        console.info("SW registered: ", registration);
       })
       .catch(registrationError => {
-        console.log("SW registration failed: ", registrationError);
+        console.warn("SW registration failed: ", registrationError);
       });
   });
 }
